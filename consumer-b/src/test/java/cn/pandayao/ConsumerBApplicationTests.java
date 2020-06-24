b518e2fa062804f784f3a31e5968c1e5d118527e
Pandayao1997 <1129794664@qq.com>
2020-06-24 14:27:51 +0800

    second commit

---
+++ consumer-b/src/test/java/cn/pandayao/ConsumerBApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.pandayao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
